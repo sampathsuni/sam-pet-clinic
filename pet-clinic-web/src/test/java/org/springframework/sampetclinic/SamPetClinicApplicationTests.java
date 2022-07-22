66fe03ce5332aac4dfe3931b20103b06c76c71fa
sampath <sampathsuni29@gmail.com>
2022-07-22 14:59:03 +0530

    fix failing text and pom.xml

---
+++ pet-clinic-web/src/test/java/org/springframework/sampetclinic/SamPetClinicApplicationTests.java
@@ -0,0 +1,15 @@
+package org.springframework.sampetclinic;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SamPetClinicApplicationTests  {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
+
+
