86dbf4c31cf1145577b857137b016bc2288642b2
sampath <sampathsuni29@gmail.com>
2022-07-26 16:36:25 +0530

    Added List all Vets on owners index Page. Closes #15

--- pet-clinic-web/src/main/java/sam/springframwork/sampetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/sam/springframwork/sampetclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package sam.springframwork.sampetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sam.springframwork.sampetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"vets/index","vets/index.html","vets"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 
