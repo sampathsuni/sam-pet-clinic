0d563741abc3df175c1e45df6605b854424880d3
sampath <sampathsuni29@gmail.com>
2022-07-27 19:31:47 +0530

    Created PetTypeService and PetTypeMapService. closes #29

---
+++ pet-clinic-data/src/main/java/sam/springframwork/sampetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package sam.springframwork.sampetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import sam.springframwork.sampetclinic.model.PetType;
+import sam.springframwork.sampetclinic.services.PetTypeService;
+
+import java.util.Set;
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
