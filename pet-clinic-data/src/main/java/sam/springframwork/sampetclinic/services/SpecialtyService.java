eaf1fc1094e68ef1e262c786e3d6a5f4109b3312
sampath <sampathsuni29@gmail.com>
2022-07-27 23:03:19 +0530

    Added SpecialtyMapService, SpecialtyService. closes #31

---
+++ pet-clinic-data/src/main/java/sam/springframwork/sampetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package sam.springframwork.sampetclinic.services;
+
+import sam.springframwork.sampetclinic.model.Speciality;
+
+public interface SpecialtyService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/sam/springframwork/sampetclinic/services/map/SpecialtyMapService.java
@@ -0,0 +1,35 @@
+package sam.springframwork.sampetclinic.services.map;
+
+import org.springframework.stereotype.Service;
+import sam.springframwork.sampetclinic.model.Speciality;
+import sam.springframwork.sampetclinic.services.SpecialtyService;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyMapService extends AbstractMapService<Speciality, Long> implements SpecialtyService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
