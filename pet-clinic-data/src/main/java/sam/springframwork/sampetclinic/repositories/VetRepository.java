6b78f0d96e0585bbc4c691c5ab97dda8f80a4134
sampath <sampathsuni29@gmail.com>
2022-08-01 15:23:28 +0530

    Created Spring Data JPA Service for Vets. closes #41

--- pet-clinic-data/src/main/java/sam/springframwork/sampetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/sam/springframwork/sampetclinic/repositories/VetRepository.java
@@ -3,5 +3,8 @@ package sam.springframwork.sampetclinic.repositories;
 import org.springframework.data.repository.CrudRepository;
 import sam.springframwork.sampetclinic.model.Vet;
 
+import java.util.Optional;
+
 public interface VetRepository extends CrudRepository<Vet, Long> {
+    Optional<Object> findAllById(Long aLong);
 }
